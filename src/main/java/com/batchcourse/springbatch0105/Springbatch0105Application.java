625aed2a499955b89827e18d12151dfa74384eb2
EdKorkollie <50729522+EdKorkollie@users.noreply.github.com>
2021-04-20 14:49:59 -0400

    First commit

---
+++ src/main/java/com/batchcourse/springbatch0105/Springbatch0105Application.java
@@ -0,0 +1,13 @@
+package com.batchcourse.springbatch0105;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbatch0105Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbatch0105Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/batchcourse/springbatch0105/Springbatch0105ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batchcourse.springbatch0105;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbatch0105ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
